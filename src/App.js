28b92f8fa27d17ac60951eaf338d51c8ab92072d
Greg Brewton <gbrewton1980@gmail.com>
2021-01-25 22:55:25 -0700

    Eliminates whitespace

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Note from "./components/note";
 function App() {
   return (
     <div id="outer-wrapper">
-      <h1> Lambda Notes </h1>
+      <h1>Lambda Notes</h1>
       <div id="App-wrapper">
         <Note />
       </div>
