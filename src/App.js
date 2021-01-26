6226fa3e91b6fd59b332e0984853d6912dc22f5c
Greg Brewton <gbrewton1980@gmail.com>
2021-01-25 22:50:47 -0700

    Styles Note component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import Note from "./components/note";
 
 function App() {
-  return (
-    <div id="outer-wrapper">
-      <h1>Note Taker</h1>
-      <div id="App-wrapper">
-        <Note />
-      </div>
-    </div>
-  );
+    return ( <
+        div id = "outer-wrapper" >
+        <
+        h1 > Lambda Notes < /h1> <
+        div id = "App-wrapper" >
+        <
+        Note / >
+        <
+        /div> <
+        /div>
+    );
 }
 
 export default App;
