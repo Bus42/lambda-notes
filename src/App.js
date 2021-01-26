27c3933682529b7161c0f21392120fdb196739c0
Greg Brewton <gbrewton1980@gmail.com>
2021-01-25 22:54:50 -0700

    Fixes VS Code linting error

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import "./App.css";
 import Note from "./components/note";
 
 function App() {
-    return ( <
-        div id = "outer-wrapper" >
-        <
-        h1 > Lambda Notes < /h1> <
-        div id = "App-wrapper" >
-        <
-        Note / >
-        <
-        /div> <
-        /div>
-    );
+  return (
+    <div id="outer-wrapper">
+      <h1> Lambda Notes </h1>
+      <div id="App-wrapper">
+        <Note />
+      </div>
+    </div>
+  );
 }
 
 export default App;
